53bf150902fbbea4eb905146c4b784172563ca88
davlsb <slawotsky@gmail.com>
2024-02-18 22:42:55 -0500

    create a function that prints who won the game

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function getComputerChoice(){
 
     return "Error";
 }
+
+function playRound(playerSelection, computerSelection) {
+    // turn playerselection to uppercase
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    // if player is rock, print one per computer selection
+    // if player is paper, print one per computer selection
+    // if player is scissors, print one per computer selection
+    switch(playerSelection){
+        case "ROCK":
+            if(computerSelection === "ROCK") return "You tied! Both chose Rock.";
+            else if(computerSelection === "PAPER") return "You Lose! Paper beats rock.";
+            else return "You won! Rock beats Scissors.";
+            break;
+        case "PAPER":
+            if(computerSelection === "PAPER") return "You tied! Both chose Paper.";
+            else if(computerSelection === "SCISSORS") return "You Lose! Scissors beats paper.";
+            else return "You won! Paper beats Rock.";
+            break;
+        case "SCISSORS":
+            if(computerSelection === "SCISSORS") return "You tied! Both chose Scissors.";
+            else if(computerSelection === "ROCK") return "You Lose! Rock beats Scissors.";
+            else return "You won! Scissors beats Paper.";
+            break;
+    }
+
+    return "Error";
+  }
